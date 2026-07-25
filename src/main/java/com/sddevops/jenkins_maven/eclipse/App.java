bd246b8634a23381cd7d10886598c92379f392c0
erdawati88 <erdawati@gmail.com>
2026-07-25 21:36:50 +0800

    feat: Update console message and pom.xml

--- src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
+++ src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Beautiful World!!" );
+        System.out.println( "Hello Beautiful World!! Build 6" );
     }
 }
